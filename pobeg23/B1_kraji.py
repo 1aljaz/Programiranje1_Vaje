578f1b2e5f0a12b8f1386b036636822942ba47f3
1aljaz <azo.marn@gmail.com>
2025-03-13 15:04:19 +0100

    vaje

--- pobeg23/B1_kraji.py
+++ pobeg23/B1_kraji.py
@@ -1,4 +1,32 @@
+from pathlib import Path
+
+def razmerje_soglasnikov(mesta:list[str]):
+    soglasniki = 'bcdfghjklmnprstvz'
+    samoglasniki = 'aeiou'
+    par = ["", 0]
+    for mesto in mesta:
+        samo = 0
+        sog = 0
+        copy = mesto
+        mesto = mesto.lower()
+        for crka in mesto:
+            if crka in soglasniki:
+                sog += 1
+            elif crka in samoglasniki:
+                samo += 1
+        try:
+            razmerje = samo / sog
+        except ZeroDivisionError:
+            continue
+        if razmerje >= par[1]:
+            par = [copy, razmerje]
+            print(par)
+    return par[0], par[1]
+
+
 ### Tega dela kode ne spreminjaj! ###
-with open('B1_kraji.txt', 'r') as datoteka:
+with open(Path(__file__).parent / 'B1_kraji.txt', 'r') as datoteka:
     kraji = datoteka.read()
     seznam_krajev = kraji.split(' ')
+
+    print(razmerje_soglasnikov(seznam_krajev))
--- pobeg23/programi.py
+++ pobeg23/programi.py
@@ -1,6 +1,8 @@
 from gesla import *
 import math
 
+# A del
+
 def vsota_del(n:int):
     kopija = n
     delitelji = []
@@ -15,39 +17,59 @@ def vsota_del(n:int):
     for st in delitelji:
     #    yield n//st
         sum+=n//st
+        delitelji.append(n//st)
+    print(delitelji)
     return sum
 
-# print(vsota_del(ORGSECRET))
+print(vsota_del(ORGSECRET))
 
 
 def eratostanovo_reseto(n:int):
-    polje = [True for i in range(n+1)]
+    polje = [True for _ in range(n+1)]
     polje[0] = polje[1] = False
     p = 2
-
+    print('zacetek')
     while (p*p <= n):
         if polje[p] == True:
             print(p)
             for i in range(p*p, n+1, p):
                 polje[i] = False
         p+=1
+    return polje
 
-def najdi_prastevilo(n:int):
+def najdi_prastevilo(n:int, secret:int):
     polje = eratostanovo_reseto(n)
-    print('Najdi')
     for i in range(n):
-        if polje[i] == True and i % 2 == 1:
+        if polje[i] and i % 2 == 1 and secret % i == 0:
             return i
 
-
-print(najdi_prastevilo(A2SECRET))
-
 def drugi_del(n:int):
+    secret = najdi_prastevilo(n, A2SECRET)
     c = n
     n *= 100
     n -= c
     s = 0
     for ch in str(n):
         s+= int(ch)
-    
+    return str(secret) + str(s)
+
+
+def collatz(n:int):
+    s = 0
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+        s += 1
     return s
+
+print(collatz(A3SECRET))
+
+# B del
+
+
+
+
+
+
