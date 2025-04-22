3e26338de047b96602439e56fb53c7590c288fc8
1aljaz <azo.marn@gmail.com>
2025-04-22 09:48:56 +0200

    vaje

--- test.py
+++ test.py
@@ -1,17 +1,10 @@
-def je_sifra(sifra):
-    sl = []
-    r = []
-    for crka, slika in sifra.items():
-        for c, s in sl:
-            if crka == s:
-                s = crka
-                continue
-        sl.append((crka, slika))
-    print(sl)
-    for i in range(len(sl)):
-        if sl[i][0] != sl[i][1]:
-            r.append(sl[i])
-    return len(r) == 0
+def neki(k, a=None):
+    if a == None:
+        a = []
+    a.append(k)
+    return a
 
-
-je_sifra({"A":"B", "B":"C", "C": "D"})
+neki(1)
+neki(2)
+neki(3)
+print(neki(4))
