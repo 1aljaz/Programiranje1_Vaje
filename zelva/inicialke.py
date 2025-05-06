a39c9fa2aa2229df175076c8aa704923dec169f2
1aljaz <azo.marn@gmail.com>
2025-05-06 09:54:43 +0200

    vaje

---
+++ zelva/inicialke.py
@@ -0,0 +1,26 @@
+import turtle as t
+
+s = t.Screen()
+s.bgcolor("light gray")
+t = t.Turtle()
+
+t.left(90)
+t.forward(100)
+t.right(135)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.right(135)
+t.forward(100)
+
+t.penup()
+t.left(90)
+t.forward(40)
+t.pendown()
+t.left(90)
+t.forward(100)
+t.right(180)
+t.forward(60)
+t.left(90)
+t.circle(30, 180)
+s.exitonclick()
